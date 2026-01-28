a9fd061bf3613347f129ee72102f3a03a4345899
Falgun Kole <147752610+Falgunkole@users.noreply.github.com>
2026-01-28 10:37:45 +0530

    Add new routes for ServiceDetail and BookService pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,28 @@
+/* src/App.tsx */
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
 import Services from './pages/Services';
+import ServiceDetail from './pages/ServiceDetail'; // New Page
+import BookService from './pages/BookService';     // New Page
 import Gallery from './pages/Gallery';
 import Contact from './pages/Contact';
 
 function App() {
   return (
     <Router>
-      <div className="min-h-screen flex flex-col">
+      <div className="min-h-screen flex flex-col bg-slate-950">
         <Header />
         <main className="flex-grow">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/services" element={<Services />} />
+            {/* Dynamic route for individual services */}
+            <Route path="/services/:serviceId" element={<ServiceDetail />} />
+            <Route path="/book" element={<BookService />} />
             <Route path="/gallery" element={<Gallery />} />
             <Route path="/contact" element={<Contact />} />
           </Routes>
